72acf689b6369c0a502e241ac0ff9bc910ff5548
MinjooBang <qkdalswn121@gmail.com>
2020-02-08 15:42:49 +0900

    Encryption

--- src/main/java/hackerRank/Encryption/Encryption.java
+++ src/main/java/hackerRank/Encryption/Encryption.java
@@ -2,20 +2,20 @@ package hackerRank.Encryption;
 
 public class Encryption{
     public String makeEncryption(String input){
-        String output = "" ;
+        String output = "";
         double norm =  Math.sqrt(input.trim().length());
         int rows = (int) Math.floor(norm);
         int colums = (int) Math.ceil(norm);
-        if(rows*colums < input.length()){
+        if(rows * colums < input.length()){
             rows = (int) Math.round(norm);
             colums = (int) Math.round(norm);
         }
-        for(int index = 0;index < colums;index++){
-            for(int index2=0;index2<rows;index2++){
-                int reStart = index2*colums+index;
-                int reLast = reStart+1;
-                if(reLast <= input.length()) {
-                    output += input.substring(reStart, reLast);
+        for(int columsNum = 0;columsNum < colums;columsNum++){
+            for(int rowNum = 0;rowNum < rows;rowNum++){
+                int reStart = rowNum * colums + columsNum;
+                int reLast  = reStart+1;
+                if(reLast <= input.length()){
+                    output += input.substring(reStart,reLast);
                 }
             }
             output += " ";
