f2925c7d8b530a83720ad2489b3e3326345130b9
MinjooBang <qkdalswn121@gmail.com>
2020-02-08 13:52:33 +0900

    CircularArrayRotation indexVer2

--- src/main/java/hackerRank/circularArrayRotation/CircularArrayRotaionIndex.java
+++ src/main/java/hackerRank/circularArrayRotation/CircularArrayRotaionIndex.java
@@ -2,12 +2,12 @@ package hackerRank.circularArrayRotation;
 
 public class CircularArrayRotaionIndex implements CircularArray {
     @Override
-    public int[] circularArrayRotation( int[] inputArray, int cycle, int[] queries) {
+    public int[] circularArrayRotation(int[] inputArray,int cycle,int[] queries) {
         int patternNum =  cycle % inputArray.length;
         int[] result = new int[queries.length];
 
-        for( int index = 0; index < queries.length; index++){
-            int reIndex = makeIndex(queries[index], patternNum, inputArray.length);
+        for(int index = 0;index < queries.length;index++){
+            int reIndex = makeIndex(queries[index],patternNum,inputArray.length);
 
             result[index] = inputArray[reIndex];
         }
@@ -15,7 +15,7 @@ public class CircularArrayRotaionIndex implements CircularArray {
         return result;
     }
 
-    private int makeIndex( int arrayIndex, int patternNum, int length){
+    private int makeIndex(int arrayIndex,int patternNum,int length){
 
         return (arrayIndex - patternNum < 0)? arrayIndex - patternNum + length : arrayIndex - patternNum;
     }
