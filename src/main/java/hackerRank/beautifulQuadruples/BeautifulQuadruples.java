9cf05a8aa78ba215f375afe99c24e4cdf56b2289
MinjooBang <qkdalswn121@gmail.com>
2020-02-15 20:49:39 +0900

    BeautifulQuadruples

--- src/main/java/hackerRank/beautifulQuadruples/BeautifulQuadruples.java
+++ src/main/java/hackerRank/beautifulQuadruples/BeautifulQuadruples.java
@@ -1,37 +1,41 @@
 package hackerRank.beautifulQuadruples;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class BeautifulQuadruples {
+    static int [] arr = new int[4];
+    static ArrayList alist = new ArrayList<>();
+    static String addString  = "";
+    static String s1="";
+    static String s2="";
+    static String s3="";
+    static String s4="";
 
     public int makeOutput(int i, int i1, int i2, int i3) {
-        int miusNum = 0;
-        int allNum = 0;
-        for (int c = 0; c <i ; c++) {
-            for (int idx = 0; idx < i1; idx++) {
-                allNum += i3 * i2;
-                for (int idx2 = 1; idx2 <= i2; idx2++) {
-                    miusNum += idx2;
-                }
-                i2--;
-            }
-        }
-        return allNum - miusNum;
-    }
 
-    public int makeOutput2(int i, int i1, int i2, int i3) {
-        int sum =0;
-        int minus = 0;
-        for( int a =0 ; a < i ; a++ ){
-            for(int b = 0 ; b < i1 ; b++ ){
-                for( int c = 0 ; c < i2 ; c++ ){
-                    for ( int d = 0 ; d < i3 ; d++ ){
+        for( int a =1 ; a <= i ; a++ ){
+            for(int b = 1 ; b <= i1 ; b++ ){
+                for( int c = 1 ; c <= i2 ; c++ ){
+                    for ( int d = 1 ; d <= i3 ; d++ ){
+
                         if ( (a ^ b ^ c ^ d) != 0 ){
-                            sum++;
+                            arr[0] = a;
+                            arr[1] = b;
+                            arr[2] = c;
+                            arr[3] = d;
+                            Arrays.sort(arr);
+
+                            addString = String.valueOf(arr[0])+String.valueOf(arr[1])
+                                    +String.valueOf(arr[2])+String.valueOf(arr[3]);
+                            alist.add(addString);
                         }
 
                     }
                 }
             }
         }
-        return sum - minus  ;
+        List<Arrays> list = new ArrayList<Arrays>(new HashSet<Arrays>(alist));
+        return list.size();
     }
 }
